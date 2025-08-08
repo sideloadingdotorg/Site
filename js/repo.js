f0fa88ff3efaafa9abf96c23c28118ef136b02c4
Virid_z <167824670+ViridDevelopment@users.noreply.github.com>
2025-08-08 23:14:13 +0100

    is this aura?

--- js/repo.js
+++ js/repo.js
@@ -45,7 +45,7 @@ function createRepoContainer(repo) {
         <div class="repo-header">
             <div class="repo-icon">
                 <svg viewBox="0 0 24 24">
-                    <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"/>
+                    <path d="M2 4h6l2 2h6a2 2 0 012 2v1M2 4v10a2 2 0 002 2h16a2 2 0 002-2V6a2 2 0 00-2-2h-6l-2-2H4a2 2 0 00-2 2z"/>
                 </svg>
             </div>
             <div class="repo-info">
